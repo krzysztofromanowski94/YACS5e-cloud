eedd54ca76cb1b6098274491607bac0c222a4758
kzysztofromanowski94 <krzysztof.romanowski94@gmail.com>
2017-10-22 22:21:54 +0200

    Basic server test

--- main.j2.go
+++ main.j2.go
@@ -5,31 +5,66 @@ import (
 	pb "github.com/krzysztofromanowski94/YACS5e-cloud/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"io"
 	"log"
 	"net"
+	"strconv"
 )
 
 var (
-	tlsCertFile = "{{ tls_cert_file }}"
-	tlsKeyFile  = "{{ tls_key_file }}"
-	port        = "{{ port }}"
+	tls, parseErr = strconv.ParseBool("{{ use_tls }}")
+	tlsCertFile   = "{{ tls_cert_file }}"
+	tlsKeyFile    = "{{ tls_key_file }}"
+	serverAddress = ":{{ server_port }}"
+	serverOpts    []grpc.ServerOption
 )
 
+type YACS5eServer struct {
+}
+
+// rpc Registration (stream Register) returns (stream Register)
+func (server *YACS5eServer) Registration(stream pb.YACS5E_RegistrationServer) error {
+	for {
+		streamIn, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			log.Println("Error in Registration Server stream in. ERROR: ", err)
+			return err
+		}
+		fmt.Println(streamIn)
+		err = stream.Send(&pb.Register{&pb.Register_Valid{true}})
+		if err != nil {
+			log.Println("Error in Registration Server stream out. ERROR: ", err)
+			return err
+		}
+	}
+}
+
+func newServer() *YACS5eServer {
+	return new(YACS5eServer)
+
+}
+
 func main() {
-	cred, err := credentials.NewServerTLSFromFile(tlsCertFile, tlsKeyFile)
+	if parseErr != nil {
+		log.Fatal("Error parsing use_tls ansible variable. ERROR:", parseErr)
+	}
+	lis, err := net.Listen("tcp", serverAddress)
 	if err != nil {
-		log.Fatal("Failed to generate credentials %v", err)
+		log.Fatal("Failed to create listen service. ERROR: ", err)
 	}
 
-	lis, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatal("Failed to create listen service %v", err)
+	if tls {
+		cred, err := credentials.NewServerTLSFromFile(tlsCertFile, tlsKeyFile)
+		if err != nil {
+			log.Fatal("Failed to generate credentials. ERROR: ", err)
+		}
+		serverOpts = append(serverOpts, grpc.Creds(cred))
 	}
 
-	server := grpc.NewServer(grpc.Creds(cred))
-
-	server.Serve(lis)
-
-	dupa := pb.Register{&pb.Register_Valid{true}}
-	fmt.Println(dupa)
+	grpcServer := grpc.NewServer(serverOpts...)
+	pb.RegisterYACS5EServer(grpcServer, newServer())
+	grpcServer.Serve(lis)
 }
