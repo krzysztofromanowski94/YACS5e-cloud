ae018560f736b3119db5ee615c8c12c2c1cf182b
Krzysztof Romanowski <krzysztof.romanowski94@gmail.com>
2017-10-30 23:06:13 +0100

    Implement getCharacter

--- proto/yacs5e.pb.go
+++ proto/yacs5e.pb.go
@@ -9,6 +9,7 @@ It is generated from these files:
 
 It has these top-level messages:
 	TUser
+	TCharacter
 	Empty
 */
 package yacs5e
@@ -73,16 +74,160 @@ func (m *TUser) GetVisibleName() string {
 	return ""
 }
 
+type TCharacter struct {
+	// Types that are valid to be assigned to Union:
+	//	*TCharacter_User
+	//	*TCharacter_Blob
+	//	*TCharacter_Id
+	Union isTCharacter_Union `protobuf_oneof:"union"`
+}
+
+func (m *TCharacter) Reset()                    { *m = TCharacter{} }
+func (m *TCharacter) String() string            { return proto.CompactTextString(m) }
+func (*TCharacter) ProtoMessage()               {}
+func (*TCharacter) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{1} }
+
+type isTCharacter_Union interface {
+	isTCharacter_Union()
+}
+
+type TCharacter_User struct {
+	User *TUser `protobuf:"bytes,5,opt,name=user,oneof"`
+}
+type TCharacter_Blob struct {
+	Blob []byte `protobuf:"bytes,6,opt,name=blob,proto3,oneof"`
+}
+type TCharacter_Id struct {
+	Id int32 `protobuf:"varint,7,opt,name=id,oneof"`
+}
+
+func (*TCharacter_User) isTCharacter_Union() {}
+func (*TCharacter_Blob) isTCharacter_Union() {}
+func (*TCharacter_Id) isTCharacter_Union()   {}
+
+func (m *TCharacter) GetUnion() isTCharacter_Union {
+	if m != nil {
+		return m.Union
+	}
+	return nil
+}
+
+func (m *TCharacter) GetUser() *TUser {
+	if x, ok := m.GetUnion().(*TCharacter_User); ok {
+		return x.User
+	}
+	return nil
+}
+
+func (m *TCharacter) GetBlob() []byte {
+	if x, ok := m.GetUnion().(*TCharacter_Blob); ok {
+		return x.Blob
+	}
+	return nil
+}
+
+func (m *TCharacter) GetId() int32 {
+	if x, ok := m.GetUnion().(*TCharacter_Id); ok {
+		return x.Id
+	}
+	return 0
+}
+
+// XXX_OneofFuncs is for the internal use of the proto package.
+func (*TCharacter) XXX_OneofFuncs() (func(msg proto.Message, b *proto.Buffer) error, func(msg proto.Message, tag, wire int, b *proto.Buffer) (bool, error), func(msg proto.Message) (n int), []interface{}) {
+	return _TCharacter_OneofMarshaler, _TCharacter_OneofUnmarshaler, _TCharacter_OneofSizer, []interface{}{
+		(*TCharacter_User)(nil),
+		(*TCharacter_Blob)(nil),
+		(*TCharacter_Id)(nil),
+	}
+}
+
+func _TCharacter_OneofMarshaler(msg proto.Message, b *proto.Buffer) error {
+	m := msg.(*TCharacter)
+	// union
+	switch x := m.Union.(type) {
+	case *TCharacter_User:
+		b.EncodeVarint(5<<3 | proto.WireBytes)
+		if err := b.EncodeMessage(x.User); err != nil {
+			return err
+		}
+	case *TCharacter_Blob:
+		b.EncodeVarint(6<<3 | proto.WireBytes)
+		b.EncodeRawBytes(x.Blob)
+	case *TCharacter_Id:
+		b.EncodeVarint(7<<3 | proto.WireVarint)
+		b.EncodeVarint(uint64(x.Id))
+	case nil:
+	default:
+		return fmt.Errorf("TCharacter.Union has unexpected type %T", x)
+	}
+	return nil
+}
+
+func _TCharacter_OneofUnmarshaler(msg proto.Message, tag, wire int, b *proto.Buffer) (bool, error) {
+	m := msg.(*TCharacter)
+	switch tag {
+	case 5: // union.user
+		if wire != proto.WireBytes {
+			return true, proto.ErrInternalBadWireType
+		}
+		msg := new(TUser)
+		err := b.DecodeMessage(msg)
+		m.Union = &TCharacter_User{msg}
+		return true, err
+	case 6: // union.blob
+		if wire != proto.WireBytes {
+			return true, proto.ErrInternalBadWireType
+		}
+		x, err := b.DecodeRawBytes(true)
+		m.Union = &TCharacter_Blob{x}
+		return true, err
+	case 7: // union.id
+		if wire != proto.WireVarint {
+			return true, proto.ErrInternalBadWireType
+		}
+		x, err := b.DecodeVarint()
+		m.Union = &TCharacter_Id{int32(x)}
+		return true, err
+	default:
+		return false, nil
+	}
+}
+
+func _TCharacter_OneofSizer(msg proto.Message) (n int) {
+	m := msg.(*TCharacter)
+	// union
+	switch x := m.Union.(type) {
+	case *TCharacter_User:
+		s := proto.Size(x.User)
+		n += proto.SizeVarint(5<<3 | proto.WireBytes)
+		n += proto.SizeVarint(uint64(s))
+		n += s
+	case *TCharacter_Blob:
+		n += proto.SizeVarint(6<<3 | proto.WireBytes)
+		n += proto.SizeVarint(uint64(len(x.Blob)))
+		n += len(x.Blob)
+	case *TCharacter_Id:
+		n += proto.SizeVarint(7<<3 | proto.WireVarint)
+		n += proto.SizeVarint(uint64(x.Id))
+	case nil:
+	default:
+		panic(fmt.Sprintf("proto: unexpected type %T in oneof", x))
+	}
+	return n
+}
+
 type Empty struct {
 }
 
 func (m *Empty) Reset()                    { *m = Empty{} }
 func (m *Empty) String() string            { return proto.CompactTextString(m) }
 func (*Empty) ProtoMessage()               {}
-func (*Empty) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{1} }
+func (*Empty) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{2} }
 
 func init() {
 	proto.RegisterType((*TUser)(nil), "TUser")
+	proto.RegisterType((*TCharacter)(nil), "TCharacter")
 	proto.RegisterType((*Empty)(nil), "Empty")
 }
 
@@ -105,10 +250,13 @@ type YACS5EClient interface {
 	Registration(ctx context.Context, in *TUser, opts ...grpc.CallOption) (*Empty, error)
 	// ERROR CODES:
 	// 110: UNKNOWN ERROR
-	// 111: INVALID LOGIN
-	// 112: INVALID PASSWORD
-	// 113: USER EXISTS
+	// 111: INVALID CREDENTIALS
 	Login(ctx context.Context, in *TUser, opts ...grpc.CallOption) (*Empty, error)
+	// ERROR CODES:
+	// 120: UNKNOWN ERROR
+	// 121: INVALID CREDENTIALS
+	// 122: CHARACTER DOES NOT EXISTS
+	GetCharacter(ctx context.Context, opts ...grpc.CallOption) (YACS5E_GetCharacterClient, error)
 }
 
 type yACS5EClient struct {
@@ -137,6 +285,37 @@ func (c *yACS5EClient) Login(ctx context.Context, in *TUser, opts ...grpc.CallOp
 	return out, nil
 }
 
+func (c *yACS5EClient) GetCharacter(ctx context.Context, opts ...grpc.CallOption) (YACS5E_GetCharacterClient, error) {
+	stream, err := grpc.NewClientStream(ctx, &_YACS5E_serviceDesc.Streams[0], c.cc, "/YACS5e/GetCharacter", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &yACS5EGetCharacterClient{stream}
+	return x, nil
+}
+
+type YACS5E_GetCharacterClient interface {
+	Send(*TCharacter) error
+	Recv() (*TCharacter, error)
+	grpc.ClientStream
+}
+
+type yACS5EGetCharacterClient struct {
+	grpc.ClientStream
+}
+
+func (x *yACS5EGetCharacterClient) Send(m *TCharacter) error {
+	return x.ClientStream.SendMsg(m)
+}
+
+func (x *yACS5EGetCharacterClient) Recv() (*TCharacter, error) {
+	m := new(TCharacter)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 // Server API for YACS5E service
 
 type YACS5EServer interface {
@@ -148,10 +327,13 @@ type YACS5EServer interface {
 	Registration(context.Context, *TUser) (*Empty, error)
 	// ERROR CODES:
 	// 110: UNKNOWN ERROR
-	// 111: INVALID LOGIN
-	// 112: INVALID PASSWORD
-	// 113: USER EXISTS
+	// 111: INVALID CREDENTIALS
 	Login(context.Context, *TUser) (*Empty, error)
+	// ERROR CODES:
+	// 120: UNKNOWN ERROR
+	// 121: INVALID CREDENTIALS
+	// 122: CHARACTER DOES NOT EXISTS
+	GetCharacter(YACS5E_GetCharacterServer) error
 }
 
 func RegisterYACS5EServer(s *grpc.Server, srv YACS5EServer) {
@@ -194,6 +376,32 @@ func _YACS5E_Login_Handler(srv interface{}, ctx context.Context, dec func(interf
 	return interceptor(ctx, in, info, handler)
 }
 
+func _YACS5E_GetCharacter_Handler(srv interface{}, stream grpc.ServerStream) error {
+	return srv.(YACS5EServer).GetCharacter(&yACS5EGetCharacterServer{stream})
+}
+
+type YACS5E_GetCharacterServer interface {
+	Send(*TCharacter) error
+	Recv() (*TCharacter, error)
+	grpc.ServerStream
+}
+
+type yACS5EGetCharacterServer struct {
+	grpc.ServerStream
+}
+
+func (x *yACS5EGetCharacterServer) Send(m *TCharacter) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func (x *yACS5EGetCharacterServer) Recv() (*TCharacter, error) {
+	m := new(TCharacter)
+	if err := x.ServerStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 var _YACS5E_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "YACS5e",
 	HandlerType: (*YACS5EServer)(nil),
@@ -207,24 +415,36 @@ var _YACS5E_serviceDesc = grpc.ServiceDesc{
 			Handler:    _YACS5E_Login_Handler,
 		},
 	},
-	Streams:  []grpc.StreamDesc{},
+	Streams: []grpc.StreamDesc{
+		{
+			StreamName:    "GetCharacter",
+			Handler:       _YACS5E_GetCharacter_Handler,
+			ServerStreams: true,
+			ClientStreams: true,
+		},
+	},
 	Metadata: "yacs5e.proto",
 }
 
 func init() { proto.RegisterFile("yacs5e.proto", fileDescriptor0) }
 
 var fileDescriptor0 = []byte{
-	// 182 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0xa9, 0x4c, 0x4c, 0x2e,
-	0x36, 0x4d, 0xd5, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x57, 0xaa, 0xe4, 0x62, 0x0d, 0x09, 0x2d, 0x4e,
-	0x2d, 0x12, 0x12, 0xe1, 0x62, 0xcd, 0xc9, 0x4f, 0xcf, 0xcc, 0x93, 0x60, 0x54, 0x60, 0xd4, 0xe0,
-	0x0c, 0x82, 0x70, 0x84, 0xa4, 0xb8, 0x38, 0x0a, 0x12, 0x8b, 0x8b, 0xcb, 0xf3, 0x8b, 0x52, 0x24,
-	0x98, 0xc0, 0x12, 0x70, 0xbe, 0x90, 0x0c, 0x17, 0x67, 0x51, 0x6a, 0x71, 0x41, 0x48, 0x7e, 0x76,
-	0x6a, 0x9e, 0x04, 0x33, 0x58, 0x12, 0x21, 0x20, 0xa4, 0xc0, 0xc5, 0x5d, 0x96, 0x59, 0x9c, 0x99,
-	0x94, 0x93, 0xea, 0x97, 0x98, 0x9b, 0x2a, 0xc1, 0x02, 0x96, 0x47, 0x16, 0x52, 0x62, 0xe7, 0x62,
-	0x75, 0xcd, 0x2d, 0x28, 0xa9, 0x34, 0x72, 0xe4, 0x62, 0x8b, 0x74, 0x74, 0x0e, 0x36, 0x4d, 0x15,
-	0x92, 0xe3, 0xe2, 0x09, 0x4a, 0x4d, 0xcf, 0x2c, 0x2e, 0x29, 0x4a, 0x2c, 0xc9, 0xcc, 0xcf, 0x13,
-	0x62, 0xd3, 0x03, 0x3b, 0x4e, 0x8a, 0x4d, 0x0f, 0xac, 0x52, 0x48, 0x9c, 0x8b, 0xd5, 0x07, 0xec,
-	0x2e, 0x34, 0x89, 0x24, 0x36, 0xb0, 0x6f, 0x8c, 0x01, 0x01, 0x00, 0x00, 0xff, 0xff, 0x29, 0xda,
-	0xd5, 0x28, 0xdd, 0x00, 0x00, 0x00,
+	// 266 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x5c, 0x90, 0x41, 0x4b, 0xc3, 0x30,
+	0x1c, 0x47, 0x97, 0xda, 0xb4, 0xee, 0xdf, 0x1e, 0x24, 0x0c, 0x0c, 0x65, 0x48, 0xe9, 0xa9, 0x07,
+	0x29, 0x32, 0xd9, 0x07, 0xd0, 0x21, 0xee, 0x20, 0x1e, 0x6a, 0x3d, 0x88, 0xa7, 0x74, 0xfd, 0x33,
+	0x83, 0x5d, 0x53, 0x92, 0x4c, 0xe9, 0xb7, 0x97, 0x45, 0xb1, 0x63, 0xc7, 0xf7, 0x5e, 0x20, 0xbf,
+	0x04, 0xe2, 0x41, 0x6c, 0xcc, 0x12, 0x8b, 0x5e, 0x2b, 0xab, 0xb2, 0x01, 0x68, 0xf5, 0x6a, 0x50,
+	0xb3, 0x19, 0xd0, 0x56, 0x6d, 0x65, 0xc7, 0x49, 0x4a, 0xf2, 0x69, 0xf9, 0x0b, 0x2c, 0x81, 0xf3,
+	0x5e, 0x18, 0xf3, 0xad, 0x74, 0xc3, 0x3d, 0x17, 0xfe, 0x99, 0xcd, 0x61, 0xaa, 0xd1, 0xf4, 0x95,
+	0xfa, 0xc4, 0x8e, 0x9f, 0xb9, 0x38, 0x0a, 0x96, 0x42, 0xf4, 0x25, 0x8d, 0xac, 0x5b, 0x7c, 0x16,
+	0x3b, 0xe4, 0xbe, 0xeb, 0xc7, 0x2a, 0x7b, 0x07, 0xa8, 0x56, 0x1f, 0x42, 0x8b, 0x8d, 0x45, 0xcd,
+	0xe6, 0xe0, 0xef, 0x0d, 0x6a, 0x4e, 0x53, 0x92, 0x47, 0x8b, 0xa0, 0x70, 0xab, 0xd6, 0x93, 0xd2,
+	0x59, 0x36, 0x03, 0xbf, 0x6e, 0x55, 0xcd, 0x83, 0x94, 0xe4, 0xf1, 0xc1, 0x1e, 0x88, 0x5d, 0x80,
+	0x27, 0x1b, 0x1e, 0xa6, 0x24, 0xa7, 0xeb, 0x49, 0xe9, 0xc9, 0xe6, 0x3e, 0x04, 0xba, 0xef, 0xa4,
+	0xea, 0xb2, 0x10, 0xe8, 0xc3, 0xae, 0xb7, 0xc3, 0x42, 0x41, 0xf0, 0x76, 0xb7, 0x7a, 0x59, 0x22,
+	0xbb, 0x82, 0xb8, 0xc4, 0xad, 0x34, 0x56, 0x0b, 0x2b, 0x55, 0xc7, 0xfe, 0xee, 0x48, 0x82, 0xc2,
+	0x9d, 0x64, 0x97, 0x40, 0x9f, 0xdc, 0xa3, 0x4f, 0xc3, 0x35, 0xc4, 0x8f, 0x68, 0xc7, 0xa9, 0x51,
+	0x31, 0xee, 0x4e, 0x8e, 0x21, 0x27, 0x37, 0xa4, 0x0e, 0xdc, 0xc7, 0xde, 0xfe, 0x04, 0x00, 0x00,
+	0xff, 0xff, 0x1e, 0x82, 0x1a, 0x0f, 0x68, 0x01, 0x00, 0x00,
 }
--- services.j2.go
+++ services.j2.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	pb "github.com/krzysztofromanowski94/YACS5e-cloud/proto"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc/status"
 	"log"
+	"runtime"
 	"strconv"
 	"strings"
 )
@@ -24,11 +26,225 @@ var (
 		"@tcp(" + sqlHostname + ":" + sqlPort + ")/" + sqlDBName
 )
 
-type YACS5eServer struct {
+// rpc Registration (User) returns (Empty)
+// ERROR CODES:
+// 100: UNKNOWN ERROR
+// 101: INVALID LOGIN
+// 102: INVALID PASSWORD
+// 103: USER EXISTS
+func (server *YACS5eServer) Registration(ctx context.Context, user *pb.TUser) (*pb.Empty, error) {
+
+	// Here should be checking if recaptcha is right
+
+	_, err := db.Exec("INSERT INTO users VALUES (NULL, ?, ?, ?)", user.Login, user.Password, user.VisibleName)
+	if err != nil {
+		switch strErr := err.Error(); {
+
+		case strings.Contains(strErr, "Error 1062"):
+			returnStr := fmt.Sprint("User ", user.Login, " exists.")
+			return &pb.Empty{}, status.Errorf(103, returnStr)
+
+		default:
+			LogUnknownError(err)
+			return &pb.Empty{}, status.Errorf(100, "Unknown error: ", err)
+		}
+	}
+
+	returnStr := fmt.Sprint("Registered user: ", user.Login)
+	return &pb.Empty{}, status.Errorf(0, returnStr)
 }
 
-func newServer() *YACS5eServer {
-	return new(YACS5eServer)
+// rpc Login (User) returns (Empty)
+// ERROR CODES:
+// 110: UNKNOWN ERROR
+// 111: INVALID CREDENTIALS
+// 112: USER NOT FOUND
+func (server *YACS5eServer) Login(ctx context.Context, user *pb.TUser) (*pb.Empty, error) {
+
+	// Here should be checking if recaptcha is right
+
+	row, err := db.Query("SELECT login, visible_name FROM users WHERE login=? AND password=? LIMIT 1", user.Login, user.Password)
+
+	if err != nil {
+		LogUnknownError(err)
+		returnStr := fmt.Sprint("UNKNOWN ERROR: ", err)
+		return &pb.Empty{}, status.Errorf(110, returnStr)
+	}
+
+	for row.Next() {
+		var (
+			login       string
+			visibleName string
+		)
+
+		err := row.Scan(&login, &visibleName)
+		if err != nil {
+			LogUnknownError(err)
+			returnStr := fmt.Sprint("UNKNOWN ERROR: ", err)
+			return &pb.Empty{}, status.Errorf(110, returnStr)
+		}
+
+		return &pb.Empty{}, status.Errorf(0, "User found")
+	}
+
+	returnStr := fmt.Sprint("User ", user.Login, " not found")
+	return &pb.Empty{}, status.Errorf(112, returnStr)
+}
+
+// ERROR CODES:
+// 120: UNKNOWN ERROR
+// 121: INVALID CREDENTIALS
+// 122: CHARACTER DOES NOT EXISTS
+// 123: ERROR GETTING DATA FROM STREAM
+// 124: ERROR SENDING DATA TO STREAM
+// 125: INCORRECT FLOW
+// 126: USER DON'T HAVE THIS CHARACTER
+func (server *YACS5eServer) GetCharacter(stream pb.YACS5E_GetCharacterServer) error {
+	var (
+		user          *pb.TUser
+		characterList CharacterList
+	)
+
+	streamIn, err := stream.Recv()
+	if err != nil {
+		returnErr := fmt.Sprint("ERROR GETTING DATA FROM INPUT STREAM: ", err)
+		log.Println(returnErr)
+		return status.Errorf(123, returnErr)
+	}
+
+	// 1. Check credentials
+
+	switch tCharacterUnion := streamIn.Union.(type) {
+
+	case *pb.TCharacter_User:
+		var (
+			userID int
+		)
+		err := db.QueryRow(
+			"SELECT id FROM users WHERE login=? AND password=? LIMIT 1",
+			tCharacterUnion.User.Login,
+			tCharacterUnion.User.Password,
+		).Scan(&userID)
+
+		switch err {
+
+		case sql.ErrNoRows:
+			// User does not exists
+			return status.Errorf(121, "INVALID CREDENTIALS")
+
+		case nil:
+			// User exists
+			user = tCharacterUnion.User
+			break
+
+		default:
+			LogUnknownError(err)
+			returnStr := fmt.Sprint("UNKNOWN ERROR: ", err)
+			return status.Errorf(120, returnStr)
+		}
+
+	default:
+		return status.Errorf(125, "EXPECTED TYPE IS TCharacter_User")
+	}
+
+	// 2. Return character list
+
+	rows, err := db.Query("SELECT id, name, data FROM characters WHERE users_id=(SELECT id FROM users WHERE login=?)", user.Login)
+	if err != nil {
+		LogUnknownError(err)
+		returnStr := fmt.Sprint("UNKNOWN ERROR: ", err)
+		return status.Errorf(120, returnStr)
+	}
+
+	for rows.Next() {
+		var (
+			id   int
+			name string
+			data []byte
+		)
+
+		err := rows.Scan(&id, &name, &data)
+		if err != nil {
+			LogUnknownError(err)
+			returnStr := fmt.Sprint("UNKNOWN ERROR: ", err)
+			return status.Errorf(120, returnStr)
+		}
+
+		// unmarshall character blob data to map[string]interface{}
+		var characterDataIF interface{}
+		err = json.Unmarshal(data, &characterDataIF)
+		if err != nil {
+			LogUnknownError(err)
+		}
+		characterData := characterDataIF.(map[string]interface{})
+
+		// add character to list
+		switch desc := characterData["description"].(type) {
+		case string:
+			characterList.Characters = append(characterList.Characters, CharacterInfo{
+				id,
+				name,
+				desc,
+			})
+		}
+	}
+
+	// Select the value of key `characters` to be send. It will result as an anonymous list.
+	blob, err := json.Marshal(characterList.Characters)
+	if err != nil {
+		LogUnknownError(err)
+		returnStr := fmt.Sprint("UNKNOWN ERROR: ", err)
+		return status.Errorf(120, returnStr)
+	}
+
+	// Send list
+	err = stream.Send(&pb.TCharacter{&pb.TCharacter_Blob{blob}})
+	if err != nil {
+		LogUnknownError(err)
+		returnStr := fmt.Sprint("ERROR SENDING DATA TO STREAM: ", err)
+		return status.Errorf(124, returnStr)
+	}
+
+	// Get character id
+	var characterId int
+
+	streamIn, err = stream.Recv()
+	if err != nil {
+		returnErr := fmt.Sprint("ERROR GETTING DATA FROM INPUT STREAM: ", err)
+		log.Println(returnErr)
+		return status.Errorf(123, returnErr)
+	}
+
+	switch tCharacterUnion := streamIn.Union.(type) {
+	case *pb.TCharacter_Id:
+		characterId = int(tCharacterUnion.Id)
+
+	default:
+		return status.Errorf(125, "EXPECTED TYPE IS TCharacter_Id")
+	}
+
+	// Get selected character from database
+	var characterData []byte
+
+	err = db.QueryRow(
+		"SELECT data FROM characters WHERE id=? AND users_id=(SELECT id FROM users WHERE login=?)",
+		characterId,
+		user.Login,
+	).Scan(&characterData)
+
+	if err == sql.ErrNoRows {
+		return status.Errorf(126, "USER DON'T HAVE THIS CHARACTER")
+	}
+
+	// Send character to client
+	err = stream.Send(&pb.TCharacter{&pb.TCharacter_Blob{characterData}})
+	if err != nil {
+		LogUnknownError(err)
+		returnStr := fmt.Sprint("ERROR SENDING DATA TO STREAM", err)
+		return status.Errorf(124, returnStr)
+	}
+
+	return status.Errorf(0, "Sent the character")
 }
 
 func init() {
@@ -50,67 +266,32 @@ func init() {
 
 	log.Println("Connection estabilished")
 
+	//testServer := newServer()
+	//
+	//log.Println(testServer.GetCharacter(nil))
+
 }
 
-// rpc Registration (User) returns (Empty)
-// ERROR CODES:
-// 100: UNKNOWN ERROR
-// 101: INVALID LOGIN
-// 102: INVALID PASSWORD
-// 103: USER EXISTS
-func (server *YACS5eServer) Registration(ctx context.Context, user *pb.TUser) (*pb.Empty, error) {
-
-	// Here should be checking if recaptcha is right
-
-	_, err := db.Exec("INSERT INTO users VALUES (NULL, ?, ?, ?)", user.Login, user.Password, user.VisibleName)
+func LogUnknownError(err error) {
 	if err != nil {
-		switch strErr := err.Error(); {
-
-		case strings.Contains(strErr, "Error 1062"):
-			returnStr := fmt.Sprint("User ", user.Login, " exists.")
-			return &pb.Empty{}, status.Errorf(103, returnStr)
-
-		default:
-			log.Fatal("Registering user caused unknown ERROR: ", err)
-			return &pb.Empty{}, status.Errorf(100, "Unknown error: ", err)
-		}
+		pc, fn, line, _ := runtime.Caller(1)
+		log.Printf("ERROR in %s[%s:%d] %v", runtime.FuncForPC(pc).Name(), fn, line, err)
 	}
-
-	returnStr := fmt.Sprint("Registered user: ", user.Login)
-	return &pb.Empty{}, status.Errorf(0, returnStr)
 }
 
-// rpc Login (User) returns (Empty)
-// ERROR CODES:
-// 110: UNKNOWN ERROR
-// 111: INVALID CREDENTIALS
-// 112: USER NOT FOUND
-func (server *YACS5eServer) Login(ctx context.Context, user *pb.TUser) (*pb.Empty, error) {
+type YACS5eServer struct {
+}
 
-	// Here should be checking if recaptcha is right
+type CharacterList struct {
+	Characters []CharacterInfo `json:"characters"`
+}
 
-	row, err := db.Query("SELECT login, visible_name FROM users WHERE login=? AND password=? LIMIT 1", user.Login, user.Password)
+type CharacterInfo struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
 
-	if err != nil {
-		returnStr := fmt.Sprint("UNKNOWN ERROR: ", err)
-		return &pb.Empty{}, status.Errorf(110, returnStr)
-	}
-
-	for row.Next() {
-		var (
-			login       string
-			visibleName string
-		)
-
-		err := row.Scan(&login, &visibleName)
-		if err != nil {
-			returnStr := fmt.Sprint("UNKNOWN ERROR: ", err)
-			return &pb.Empty{}, status.Errorf(110, returnStr)
-		}
-
-		return &pb.Empty{}, status.Errorf(0, "User found")
-	}
-
-	returnStr := fmt.Sprint("User ", user.Login, " not found")
-	return &pb.Empty{}, status.Errorf(112, returnStr)
+func newServer() *YACS5eServer {
+	return new(YACS5eServer)
 }
