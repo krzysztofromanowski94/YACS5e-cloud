7c9b6d6e6b324ffdce79fdabed40b53f53976464
krzysztofromanowski94 <krzysztof.romanowski94@gmail.com>
2017-11-19 22:15:11 +0100

    Try with nginx ssl service
    Signed-off-by: krzysztofromanowski94 <krzysztof.romanowski94@gmail.com>

--- main.j2.go
+++ main.j2.go
@@ -5,23 +5,23 @@ import (
 	"google.golang.org/grpc"
 	"log"
 	"net"
-	"strconv"
 	//"golang.org/x/crypto/acme/autocert"
 	//"golang.org/x/crypto/acme"
 	//"google.golang.org/genproto/googleapis/monitoring/v3"
 	//"crypto/tls"
-	"crypto/tls"
-	"github.com/krzysztofromanowski94/YACS5e-cloud/utils"
-	"os"
+	//"crypto/tls"
+	//"github.com/krzysztofromanowski94/YACS5e-cloud/utils"
+	//"os"
+	//"google.golang.org/grpc/credentials"
 )
 
 var (
-	useTls, parseErr = strconv.ParseBool("{{ use_tls }}")
+	//useTls, parseErr = strconv.ParseBool("{{ use_tls }}")
 	//tlsCertFile      = "{{ tls_cert_file }}"
 	//tlsKeyFile       = "{{ tls_key_file }}"
 	serverAddress = ":{{ server_port }}"
 	serverOpts    []grpc.ServerOption
-	lis           net.Listener
+	//lis           net.Listener
 
 	exitProgramChannel chan bool = make(chan bool, 1)
 )
@@ -33,37 +33,37 @@ func main() {
 	//
 	//
 
-	if parseErr != nil {
-		log.Fatal("Error parsing use_tls ansible variable. ERROR:", parseErr)
-	}
-	//lis, err := net.Listen("tcp", serverAddress)
-	//if err != nil {
-	//	log.Fatal("Failed to create listen service. ERROR: ", err)
+	//if parseErr != nil {
+	//	log.Fatal("Error parsing use_tls ansible variable. ERROR:", parseErr)
 	//}
-
-	if useTls {
-		//cred, err := credentials.NewServerTLSFromFile(tlsCertFile, tlsKeyFile)
-		//if err != nil {
-		//	log.Fatal("Failed to generate credentials. ERROR: ", err)
-		//}
-		//serverOpts = append(serverOpts, grpc.Creds(cred))
-		/*serverOpts = append(serverOpts,*/
-
-		//ttt := &tls.Config{GetCertificate: asd.GetCertificate}
-
-		tlsSetting := utils.GetTLS("ptp-thingers.pl", "/root/secret/certs")
-		//creds := credentials.NewTLS(tls)
-
-		var err error
-
-		lis, err = tls.Listen("tcp", serverAddress, tlsSetting)
-		if err != nil {
-			utils.LogUnknownError(err)
-			os.Exit(1)
-		}
-
-		//serverOpts = append(serverOpts, grpc.Creds(creds))
+	lis, err := net.Listen("tcp", serverAddress)
+	if err != nil {
+		log.Fatal("Failed to create listen service. ERROR: ", err)
 	}
+
+	//if useTls {
+	//cred, err := credentials.NewServerTLSFromFile(tlsCertFile, tlsKeyFile)
+	//if err != nil {
+	//	log.Fatal("Failed to generate credentials. ERROR: ", err)
+	//}
+	//serverOpts = append(serverOpts, grpc.Creds(cred))
+	/*serverOpts = append(serverOpts,*/
+
+	//ttt := &tls.Config{GetCertificate: asd.GetCertificate}
+
+	//tlsSetting := utils.GetTLS("ptp-thingers.pl", "/root/secret/certs")
+	//creds := credentials.NewTLS(tls)
+	//
+	//var err error
+	//
+	//lis, err = tls.Listen("tcp", serverAddress, tlsSetting)
+	//if err != nil {
+	//	utils.LogUnknownError(err)
+	//	os.Exit(1)
+	//}
+	//
+	//serverOpts = append(serverOpts, grpc.Creds(creds))
+	//}
 
 	grpcServer := grpc.NewServer(serverOpts...)
 	pb.RegisterYACS5EServer(grpcServer, newServer())
